934200df2cd1fde80e314c849075f6ab97d0cecf
hfyu <hfyu@Ctrip.com>
2015-02-28 16:45:50 +0800

    add fis-pkg

---
+++ fis-conf.js
@@ -0,0 +1,39 @@
+fis.config.merge({
+    project:{
+        charest:"utf8",
+        md5Length : 0,
+        include: ["src/**","mods/**"]
+    },
+    modules : {
+        //压缩
+        optimizer:{
+            js:"uglify-js"
+        }
+    },
+    settings:{
+        //fis-optimizer-uglify-js插件的配置数据
+        optimizer:{
+            'uglify-js' : {
+                output : {
+                    max_line_len : 500,
+                    ascii_only: true
+                }
+            }
+        }
+    },
+    pack : {
+        'pkg/farman.libs.js' : 'src/libs/**.js',
+        'pkg/farman.seajs.js' : [
+            "src/common/**.js",
+            "src/core/**.js",
+            "src/page/**.js",
+            "src/app.js"    
+        ],
+        'pkg/farman.page.js' : [
+            "mods/routes.js",
+            "mods/common/**.js",
+            "mods/page/**.js"
+        ],
+        'pkg/farman.all.js' : '**.js'
+    }
+});
